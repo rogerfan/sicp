dbf9c27d5b65bb08d4c82cca45d6866cc9ab1c43
Roger Fan <rogerfan.rf@gmail.com>
2014-06-27 10:46:18 -0500

    Finished 1-2, though did not do exercises requiring random or runtime primitives due to lack of them in the online REPL I am using.

--- ch1-2.scm
+++ ch1-2.scm
@@ -150,3 +150,58 @@
                           q
                           (- count 1)))))
   (fib-iter 1 0 0 1 n))
+
+;; 1.2.5 - Greatest Common Divisors
+(define (remainder a b)
+  (- a (* b (floor (/ a b))))
+
+(define (gcd a b)
+  (cond ((= b 0) a)
+        (else (gcd b (remainder a b)))))
+
+;; 1.2.6 - Testing for Primality
+(define (smallest-divisor n)
+  (find-divisor n 2))
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+(define (divides? a b)
+  (= (remainder b a) 0))
+(define (remainder a b)
+  (- a (* b (floor (/ a b)))))
+(define (square x) (* x x))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+; Fermat Test
+(define (expmod base exp m)
+  (cond ((- exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-print? n (- times 1)))
+        (else false)))
+
+;; Ex 1.23
+(define (smallest-divisor n)
+  (find-divisor n 2))
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (next test-divisor)))))
+(define (next n)
+  (cond ((= n 2) 3)
+        (else (+ n 2))))
