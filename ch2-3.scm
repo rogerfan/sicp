6ebe5e0e1bbb28c835e389f0157ee9bcb7cf6741
Roger Fan <rogerfan.rf@gmail.com>
2014-07-18 16:49:54 -0500

    Finished 2.3.2 - Symbolic Differentiation.

--- ch2-3.scm
+++ ch2-3.scm
@@ -12,21 +12,16 @@
                          (equal? (cdr l1) (cdr l2))))))
 
 ;; 2.3.2 - Symbolic Differentiation
-
-;Primitives:
-;(variable? e) Is e a variable?
-;(same-variable? v1 v2)  Are v1 and v2 the same variable?
-;(sum? e) Is e a sum?
-;(addend e)  Addend of the sum e.
-;(augend e)  Augend of the sum e.
-;(make-sum a1 a2)  Construct the sum of a1 and a2.
-;(product? e) Is e a product?
-;(multiplier e)  Multiplier of the product e.
-;(multiplicand e)  Multiplicand of the product e.
-;(make-product m1 m2)  Construct the product of m1 and m2.
+;; Ex 2.56
+;; Ex 2.57
 
 (define (eqnumber? exp num)
   (and (number? exp) (= exp num)))
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
 
 (define (variable? x) (symbol? x))
 (define (same-variable? v1 v2)
@@ -42,14 +37,26 @@
         ((eqnumber? m2 1) m1)
         ((and (number? m1) (number? m2)) (* m1 m2))
         (else (list '* m1 m2))))
+(define (make-exponent base exp)
+  (cond ((eqnumber? exp 0) 1)
+        ((eqnumber? exp 1) base)
+        ((eqnumber? base 1) 1)
+        ((and (number? base) (number? exp)) (expt base exp))
+        (else (list '** base exp))))
 (define (sum? x)
   (and (pair? x) (eq? (car x) '+)))
 (define (addend s) (cadr s))
-(define (augend s) (caddr s))
+(define (augend s)
+  (accumulate make-sum 0 (cddr s)))
 (define (product? x)
   (and (pair? x) (eq? (car x) '*)))
 (define (multiplier p) (cadr p))
-(define (multiplicand p) (caddr p))
+(define (multiplicand p)
+ (accumulate make-product 1 (cddr p)))
+(define (exponent? x)
+  (and (pair? x) (eq? (car x) '**)))
+(define (base e) (cadr e))
+(define (exponent e) (caddr e))
 
 (define (deriv exp var)
   (cond ((number? exp) 0)
@@ -64,4 +71,11 @@
                          (deriv (multiplicand exp) var))
            (make-product (deriv (multiplier exp) var)
                          (multiplicand exp))))
+        ((exponent? exp)
+         (make-product
+           (make-product
+             (exponent exp)
+             (make-exponent (base exp)
+                            (make-sum (exponent exp) -1)))
+           (deriv (base exp) var)))
         (else (error "Unknown expression type -- DERIV" exp))))
